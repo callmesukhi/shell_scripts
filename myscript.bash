b7042c1d72292160b337f8dba6e875234bc3b1a1
Sukhmander Singh <callmesukhi@gmail.com>
2022-05-10 17:53:10 +0200

    First script

---
+++ myscript.bash
@@ -0,0 +1,16 @@
+""" To run: chmod +x ~/Desktop/myFolder/myscript.bash
+
+Commands for files:
+mkdir - Command to use to create a new directory
+mv - Command to use move a file from one location to another
+rm   - Command to use to remove/delete a file
+rmdir - Command to use to remove/delete a directory
+cat - Command to use to read the files
+less - Command to use to read/view the longer files
+
+
+Variables:
+Named placeholders that refers to a certain value.
+"""
+
+#!/bin/bash
